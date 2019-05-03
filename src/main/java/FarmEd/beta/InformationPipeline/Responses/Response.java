bb5ddb5bfb29e894902473ae51009d3eba5a20c6
Hakanoreaver <hakanoreaver12@hotmail.com>
2019-05-03 22:35:43 +1000

    no message

--- src/main/java/FarmEd/beta/InformationPipeline/Responses/Response.java
+++ src/main/java/FarmEd/beta/InformationPipeline/Responses/Response.java
@@ -1,9 +1,11 @@
 package FarmEd.beta.InformationPipeline.Responses;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class Response {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
