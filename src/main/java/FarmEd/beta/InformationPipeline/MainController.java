8f5852c158dc6a202e51414cc56c3cb68fc8b65b
Hakanoreaver <hakanoreaver12@hotmail.com>
2019-05-13 12:19:05 +1000

    no message

--- src/main/java/FarmEd/beta/InformationPipeline/MainController.java
+++ src/main/java/FarmEd/beta/InformationPipeline/MainController.java
@@ -148,7 +148,7 @@ public class MainController {
     }
 
     @CrossOrigin(origins = "https://agriculturepipeline.com", allowedHeaders = "*", allowCredentials = "true")
-    @PostMapping(path="query/{queryNum}/")
+    @PostMapping(path="query/find/{queryNum}/")
     public @ResponseBody
     Optional<Question> getQuestion(@PathVariable int queryNum) {
         Optional<Question> temp = queryRepository.findById(queryNum);
