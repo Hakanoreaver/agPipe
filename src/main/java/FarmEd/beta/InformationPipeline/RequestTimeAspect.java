866654d1cda5197232b0308c50aa429f366baae5
Hakanoreaver <hakanoreaver12@hotmail.com>
2019-05-28 13:53:45 +1000

    no message

--- src/main/java/FarmEd/beta/InformationPipeline/RequestTimeAspect.java
+++ src/main/java/FarmEd/beta/InformationPipeline/RequestTimeAspect.java
@@ -3,8 +3,6 @@ package FarmEd.beta.InformationPipeline;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 
